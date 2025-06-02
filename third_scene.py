215067705d737e9cebc50e3bbe2409880a8b41de
Chris Wuthrich <christian.wuthrich@gmail.com>
2025-06-02 12:54:13 +0100

    third scene conjecture

--- third_scene.py
+++ third_scene.py
@@ -11,18 +11,43 @@ class ThirdScene(Scene):
 
         te_compare = Text("Compare")
         te_compare.shift(2*LEFT)
-        te_nt = Tex(r"$\mathcal{N}(T) = $ bounded rational solutions")
+        te_nt1 = Tex(r"$\mathcal{N}(T) = $")
+        te_nt2 = Text("bounded rational \n solutions")
+        te_nt = VGroup(te_nt1,te_nt2)
+        te_nt.arrange(RIGHT)
         te_nt.shift(2*RIGHT+1*UP)
-        te_mq = Tex(r"$\mathcal{M}(Q) = $",  " solutions modulo ", r"$Q$")
+        te_mq1 = Tex(r"$\mathcal{M}(Q) = $")
+        te_mq2 = Tex("solutions \n modulo ", r"$Q$")
+        te_mq = VGroup(te_mq1,te_mq2)
+        te_mq.arrange(RIGHT)
         te_mq.shift(2*RIGHT+1*DOWN)
-        te_mq.set_color_by_tex("Q", RED)
+        # te_mq.set_color_by_tex("Q", RED)
         self.add(te_compare, te_nt, te_mq)
         self.wait(2)
 
+        te_qt = Tex(r"$Q = T! = 1 \cdot 2 \cdots T$")
+        te_qt.shift(2*RIGHT+2*DOWN)
+        self.add(te_mq)
+        self.wait(2)
+
         self.remove(te_compare)
+        te_lim = Tex(r"$\lim_{T\to\infty} \frac{T!\cdot \mathcal{N}(T)^2}{\mathcal{M}(T!)}$")
+        te_lim.scale(2)
+        te_lim.shift(2*LEFT)
+        self.add(te_lim)
+        self.wait()
 
+        self.remove(te_mq, te_qt, te_nt)
+        self.play(te_lim.animate.move_to(ORIGIN), run_time=1)
+        self.wait(1)
 
-
+        te_con = Text("Conjecture", color=YELLOW)
+        te_con.shift(2*UP+3*LEFT)
+        self.add(te_con)
+        te_conv = Text("converges to a positive real number.")
+        te_conv.shift(2*DOWN)
+        self.add(te_conv)
+        self.wait(2)
 
 
 #  now render it
