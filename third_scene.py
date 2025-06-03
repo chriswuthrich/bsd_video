a99cf6681a6061bf909fedc2cded41a382f7e29f
Chris Wuthrich <christian.wuthrich@gmail.com>
2025-06-03 22:28:19 +0100

    adjust placement of text

--- third_scene.py
+++ third_scene.py
@@ -24,10 +24,10 @@ def my_background():
         fill_opacity=1,
     )
     gradient_rect.set_fill(
-        color=[rgb_to_color([0.0, 0.0, 0.2]), BLACK],
+        color=[rgb_to_color([0.0, 0.0, 0.3]), BLACK],
         opacity=1
     )
-    gradient_rect.rotate(PI / 2)
+    gradient_rect.rotate(PI/2)
     return gradient_rect
 
 
@@ -36,6 +36,13 @@ def limit_expression():
     Constructs the main limit expression
     $\lim_{T\to\infty} \frac{T!\cdot \mathcal{N}(T)^2}{\mathcal{M}(T!)}$
     as a group of objects
+
+    [0] is lim_T->oo
+    [1][0][0] is T!
+    [1][0][1] is cdot
+    [1][0][2] is N(T)^2
+    [1][1] is the fraction line
+    [1][2] is M(T!)
     """
     # Tex(r"$\lim_{T\to\infty} \frac{T!\cdot \mathcal{N}(T)^2}{\mathcal{M}(T!)}$")
     v = VGroup()
@@ -79,32 +86,43 @@ class ThirdScene(Scene):
         te_compare.shift(2*LEFT)
 
         rhs = VGroup()
-
-        te_nt1 = Tex(r"$\mathcal{N}(T) = $")
-        te_nt2 = Text("bounded rational \n solutions")
+        te_nt1 = Tex(r"$\mathcal{N}(T) = \# $")
+        te_nt2 = Text("bounded \n" + "rational \n" + "solutions").scale(.8)
         te_nt = VGroup(te_nt1,te_nt2)
-        te_nt.arrange(RIGHT)
+        te_nt.arrange(RIGHT, buff=0.2)
 
-        te_mq1 = Tex(r"$\mathcal{M}(Q) = $")
-        te_mq2 = Text(r"solutions \n modulo ")
-        te_mq3 = Tex(r"$Q$")
-        te_mq = VGroup(te_mq1,te_mq2, te_mq3)
-        te_mq.arrange(RIGHT, buff=0)
+        te_mq1 = Tex(r"$\mathcal{M}(Q) = \# $")
+        te_mq2 = Text("solutions").scale(0.8)
+        te_mq3 = Text("modulo ").scale(.8)
+        te_mq4 = Tex(r"$Q$")
+        te_mqa = VGroup(te_mq3, te_mq4)
+        te_mqa.arrange(RIGHT, buff=.2)
+        te_mqa[1].shift(0.05 * DOWN)
+        te_mqb = VGroup(te_mq2,te_mqa)
+        te_mqb.arrange(DOWN, buff=0.1, aligned_edge=LEFT)
+        te_mq = VGroup(te_mq1,te_mqb)
+        te_mq.arrange(RIGHT, buff=0.2)
 
         te_qt = Tex(r"$Q = T! = 1 \cdot 2 \cdots T$")
         rhs.add(te_nt,te_mq,te_qt)
         rhs.arrange(DOWN, buff=1.1, aligned_edge=LEFT)
-        rhs.shift(3*RIGHT)
+        rhs.shift(3.5*RIGHT)
         self.add(rhs)
         self.wait(2)
 
         self.remove(te_compare)
         te_lim = limit_expression()  #  Tex(r"$\lim_{T\to\infty} \frac{T!\cdot \mathcal{N}(T)^2}{\mathcal{M}(T!)}$")
         te_lim.scale(2)
-        te_lim.shift(2*LEFT)
+        te_lim.shift(3*LEFT)
+        self.add(te_lim[1][1])
+        self.wait(1)
+        self.add(te_lim[1][0][2])
+        self.wait(1)
         self.add(te_lim[1][2])
         self.wait(1)
-        self.add(te_lim)
+        self.add(te_lim[1][0])
+        self.wait(1)
+        self.add(te_lim[0])
         self.wait(1)
 
         self.remove(te_mq, te_qt, te_nt)
