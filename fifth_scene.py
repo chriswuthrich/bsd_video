21bcd298445794b2d6ec8ff414aca8474c472f8e
Chris Wuthrich <christian.wuthrich@gmail.com>
2025-07-18 14:06:01 +0100

    start on 5, update script

--- fifth_scene.py
+++ fifth_scene.py
@@ -21,7 +21,24 @@ import json
 class FifthScene(Scene):
 
     def construct(self):
-        pass
+
+        # 5 Rank and original conjecture
+        # 5.1 Rank
+        self.next_section("5.1 What is the rank")
+        st = StudentChar()
+        te = StudentChar(height=1.2, width=0.8, colour=GREEN, lid_colour=DARK_GRAY)
+        st.scale(1)
+        te.scale(1)
+        stte = VGroup(st, te)
+        stte.arrange()
+        stte.to_corner(DL)
+        shz(stte, 10)
+        self.add(my_background(), stte)
+
+        # state Theorem
+        thm_tit = Text("Theorem:", color=YELLOW)
+
+
 
 
 #  now render it
