2c827472aae25ab610290b2f5284a2cb57561f46
Chris Wuthrich <christian.wuthrich@gmail.com>
2026-01-30 16:07:16 +0000

    ff

--- splitup/ellipticcurves2.py
+++ splitup/ellipticcurves2.py
@@ -122,9 +122,9 @@ class EllipticCurves2_with_text(Scene):
 # now render it
 if __name__ == "__main__":
     config.renderer = "cairo"
-    config.format = "mov"
+    config.format = "png"
     config.transparent = True
-    config.write_to_movie = True
+    config.write_to_movie = False
 
     # Optional but recommended
     config.background_color = None
